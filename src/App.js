3bc5ff48d9643521d834a7f3effed6119508f74d
Imaad Mohammed <imaadmohammed@gmail.com>
2022-05-30 22:11:58 -0700

    added tabs

--- src/App.js
+++ src/App.js
@@ -8,32 +8,64 @@ import {
   Flex,
   Button,
   Spacer,
+  Divider,
+  VStack,
+  TabList,
+  Tabs,
+  Tab,
+  TabPanels,
+  TabPanel,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function NavBar(props) {
-
   return (
-    <Flex>
-      <HStack>
-        <Button colorScheme={'blue'}>Button 1</Button>
-        <Button colorScheme={'red'}>Button 2</Button>
-      </HStack>
-      <Spacer></Spacer>
-      <ColorModeSwitcher />
+    <Flex align={"right"} m={5}>
+      <Text>Imaad Mohammed</Text>
+      <Spacer />
+      <Text>More</Text>
     </Flex>
+  );
+}
+
+
+
+function AboutMe(props) {
+  return (
+    <VStack>
+      <Text fontSize={"2xl"}>Skills</Text>
+    </VStack>
+  );
+}
+function TabBody(props) {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>About Me</Tab>
+        <Tab>Page 2</Tab>
+        <Tab>Page 3</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel><AboutMe /></TabPanel>
+        <TabPanel><Text fontSize={"2xl"}>Test2</Text></TabPanel>
+        <TabPanel>Test3</TabPanel>
+      </TabPanels>
+    </Tabs>);
+}
+function Body(props) {
+  return (
+    <VStack>
+      <Text m={5} fontSize={"3xl"}>I'm Imaad Mohammed, a Computer Science Graduate from Cal Poly Pomona!</Text>
+      <TabBody/>
+    </VStack>
   );
 }
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <NavBar></NavBar>
-      <Box textAlign="center" fontSize="xl">
-        <Text>
-          Imaad Mohammed
-        </Text>
-      </Box>
+      <NavBar />
+      <Divider />
+      <Body />
     </ChakraProvider>
   );
 }
